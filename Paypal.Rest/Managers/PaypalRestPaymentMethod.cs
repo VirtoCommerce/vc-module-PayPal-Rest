f5598b925a834d74c3ec5663abbce71269c898d9
Montane Hamilton <montane.hamilton@cireson.com>
2017-06-23 17:59:15 -0600

    Module uploads without error.

--- Paypal.Rest/Managers/PaypalRestPaymentMethod.cs
+++ Paypal.Rest/Managers/PaypalRestPaymentMethod.cs
@@ -3,67 +3,21 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using PayPal.PayPalAPIInterfaceService;
-using PayPal.PayPalAPIInterfaceService.Model;
+using PayPal.Api;
 using VirtoCommerce.Domain.Payment.Model;
 
 namespace Paypal.Rest.Managers
 {
     public class PaypalRestPaymentMethod : PaymentMethod
     {
-        private const string _paypalApiModeStoreSetting = "Paypal.DirectPayments.Mode";
-        private const string _paypalApiUserNameStoreSetting = "Paypal.DirectPayments.APIUsername";
-        private const string _paypalApiPasswordStoreSetting = "Paypal.DirectPayments.APIPassword";
-        private const string _paypalApiSignatureStoreSetting = "Paypal.DirectPayments.APISignature";
-
         public PaypalRestPaymentMethod()
-            : base("Paypal.DirectPayment")
+            : base("Paypal.Rest")
         {
         }
 
-        private string Mode
-        {
-            get
-            {
-                return GetSetting(_paypalApiModeStoreSetting);
-            }
-        }
+        public override PaymentMethodType PaymentMethodType => PaymentMethodType.Standard;
 
-        private string APIUsername
-        {
-            get
-            {
-                return GetSetting(_paypalApiUserNameStoreSetting);
-            }
-        }
-
-        private string APIPassword
-        {
-            get
-            {
-                return GetSetting(_paypalApiPasswordStoreSetting);
-            }
-        }
-
-        private string APISignature
-        {
-            get
-            {
-                return GetSetting(_paypalApiSignatureStoreSetting);
-            }
-        }
-
-
-        public override PaymentMethodType PaymentMethodType
-        {
-            get { return PaymentMethodType.Standard; }
-        }
-
-        public override PaymentMethodGroupType PaymentMethodGroupType
-        {
-            get { return PaymentMethodGroupType.BankCard; }
-        }
+        public override PaymentMethodGroupType PaymentMethodGroupType => PaymentMethodGroupType.BankCard;
 
         public override ProcessPaymentResult ProcessPayment(ProcessPaymentEvaluationContext context)
         {
@@ -75,46 +29,115 @@ namespace Paypal.Rest.Managers
 
             var retVal = new ProcessPaymentResult();
 
-            DoDirectPaymentResponseType response = null;
-            string error;
-            bool isSuccess = false;
-            var doDirectPaymentRequest = GetDoDirectPaymentRequest(context);
-            try
+            var config = new Dictionary<string, string>
             {
-                var service = GetService();
-                response = service.DoDirectPayment(doDirectPaymentRequest);
+                { "mode", GetSetting("PayPal.Rest.Mode") },
+                { "clientId", GetSetting("PayPal.Rest.ClientId") },
+                { "clientSecret", GetSetting("PayPal.Rest.ClientSecret") }
+            };
+            
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken);
 
-                isSuccess = CheckResponse(response, out error);
-                context.Payment.IsApproved = true;
-            }
-            catch (PayPal.Exception.ConnectionException ex)
+            var shippingAddress =
+                context.Order.Addresses.FirstOrDefault(
+                    a => a.AddressType == VirtoCommerce.Domain.Commerce.Model.AddressType.BillingAndShipping || a.AddressType == VirtoCommerce.Domain.Commerce.Model.AddressType.Shipping);
+            if (shippingAddress == null)
             {
-                error = ((PayPal.Exception.ConnectionException)ex.InnerException).Response;
+                throw new Exception("No shipping address available.");
             }
+
+            var transaction = new Transaction
+            {
+                amount = new Amount
+                {
+                    currency = "USD",
+                    total = context.Order.Total.ToString(CultureInfo.InvariantCulture),
+                    details = new Details
+                    {
+                        shipping = context.Order.ShippingSubTotal.ToString(CultureInfo.InvariantCulture),
+                        subtotal = context.Order.SubTotal.ToString(CultureInfo.InvariantCulture),
+                        tax = context.Order.TaxTotal.ToString(CultureInfo.InvariantCulture),
+                    }
+                },
+                description = $"Order from {context.Store.Name}.",
+                item_list = new ItemList
+                {
+                    shipping_address = new ShippingAddress
+                    {
+                        city = shippingAddress.City,
+                        country_code = shippingAddress.CountryCode,
+                        line1 = shippingAddress.Line1,
+                        line2 = shippingAddress.Line2,
+                        phone = shippingAddress.Phone,
+                        postal_code = shippingAddress.PostalCode,
+                        state = shippingAddress.RegionId,
+                    }
+                }
+            };
+
+            var payer = new Payer
+            {
+                payment_method = "credit_card",
+                funding_instruments = new List<FundingInstrument>
+                {
+                    new FundingInstrument
+                    {
+                        credit_card = new CreditCard
+                        {
+                            billing_address = new Address
+                            {
+                                city = context.Payment.BillingAddress.City,
+                                country_code = context.Payment.BillingAddress.CountryCode,
+                                line1 = context.Payment.BillingAddress.Line1,
+                                line2 = context.Payment.BillingAddress.Line2,
+                                phone = context.Payment.BillingAddress.Phone,
+                                postal_code = context.Payment.BillingAddress.PostalCode,
+                                state = context.Payment.BillingAddress.RegionId,
+                            },
+                            cvv2 = context.BankCardInfo.BankCardCVV2,
+                            expire_month = context.BankCardInfo.BankCardMonth,
+                            expire_year = context.BankCardInfo.BankCardYear,
+                            first_name = context.Payment.BillingAddress.FirstName,
+                            last_name = context.Payment.BillingAddress.LastName,
+                            number = context.BankCardInfo.BankCardNumber,
+                            type = context.BankCardInfo.BankCardType
+                        }
+                    }
+                },
+                payer_info = new PayerInfo
+                {
+                    email = context.Order.CustomerId // Look up customer using this.
+                }
+            };
+            var payment = new Payment
+            {
+                intent = "Sale",
+                payer = payer,
+                transactions = new List<Transaction> { transaction}
+            };
+            var createdPayment = payment.Create(apiContext);
 
             PaymentStatus newStatus;
-
-            if (isSuccess)
+            if (createdPayment.state == "failed")
             {
-                retVal.OuterId = context.Payment.OuterId = response.TransactionID;
-                retVal.IsSuccess = (response.Ack.Value == AckCodeType.SUCCESS || response.Ack.Value == AckCodeType.SUCCESSWITHWARNING);
-                if (retVal.IsSuccess)
-                {
-                    context.Payment.CapturedDate = DateTime.UtcNow;
-                    newStatus = PaymentStatus.Paid;
-                }
-                else
-                {
-                    context.Payment.VoidedDate = DateTime.UtcNow;
-                    newStatus = PaymentStatus.Voided;
-                }
+                context.Payment.IsApproved = false;
+                var error = createdPayment.failure_reason;
+
+                retVal.Error = error;
+                context.Payment.VoidedDate = DateTime.UtcNow;
+                newStatus = PaymentStatus.Voided;
             }
             else
             {
-                retVal.Error = error;
-                newStatus = PaymentStatus.Voided;
-            }
+                context.Payment.IsApproved = true;
 
+                retVal.OuterId = createdPayment.id;
+                retVal.IsSuccess = true;
+                context.Payment.CapturedDate = DateTime.UtcNow;
+                newStatus = PaymentStatus.Paid;
+            }
+            
             retVal.NewPaymentStatus = context.Payment.PaymentStatus = newStatus;
             return retVal;
         }
@@ -143,202 +166,5 @@ namespace Paypal.Rest.Managers
         {
             return new ValidatePostProcessRequestResult();
         }
-
-        #region Private methods
-
-        private PayPalAPIInterfaceServiceService GetService()
-        {
-            var config = new Dictionary<string, string>();
-
-            var isSandbox = Mode.ToLower().Equals("sandbox");
-
-            var url = isSandbox ? "https://api-3t.sandbox.paypal.com/2.0" : "https://api-3t.paypal.com/2.0";
-
-            config.Add("PayPalAPI", url);
-            config.Add("mode", Mode);
-            config.Add("account0.apiUsername", APIUsername);
-            config.Add("account0.apiPassword", APIPassword);
-            config.Add("account0.apiSignature", APISignature);
-
-            return new PayPalAPIInterfaceServiceService(config);
-        }
-
-        private DoDirectPaymentReq GetDoDirectPaymentRequest(ProcessPaymentEvaluationContext context)
-        {
-            var retVal = new DoDirectPaymentReq();
-
-            retVal.DoDirectPaymentRequest = new DoDirectPaymentRequestType();
-            retVal.DoDirectPaymentRequest.Version = GetApiVersion();
-            var details = new DoDirectPaymentRequestDetailsType();
-            retVal.DoDirectPaymentRequest.DoDirectPaymentRequestDetails = details;
-            details.PaymentAction = PaymentActionCodeType.SALE;
-
-            //credit card
-            details.CreditCard = GetCreditCardDetails(context);
-
-
-            //order totals
-            details.PaymentDetails = new PaymentDetailsType();
-            details.PaymentDetails.OrderTotal = new BasicAmountType();
-            details.PaymentDetails.OrderTotal.value = FormatMoney(context.Payment.Sum);
-            details.PaymentDetails.OrderTotal.currencyID = (CurrencyCodeType)Enum.Parse(typeof(CurrencyCodeType), context.Order.Currency.ToString());
-            details.PaymentDetails.Custom = context.Order.Id;
-            details.PaymentDetails.ButtonSource = "Virto_SP";
-
-            return retVal;
-        }
-
-        private CreditCardDetailsType GetCreditCardDetails(ProcessPaymentEvaluationContext context)
-        {
-            var retVal = new CreditCardDetailsType();
-
-            retVal.CreditCardNumber = context.BankCardInfo.BankCardNumber;
-            retVal.CreditCardType = GetPaypalCreditCardType(context.BankCardInfo.BankCardType);
-            retVal.ExpMonth = context.BankCardInfo.BankCardMonth;
-            retVal.ExpYear = (context.BankCardInfo.BankCardYear < 100 ? 2000 : 0) + context.BankCardInfo.BankCardYear;
-            retVal.CVV2 = context.BankCardInfo.BankCardCVV2;
-            retVal.CardOwner = new PayerInfoType();
-
-            if (context.Order.Addresses.Any(x => x.AddressType == VirtoCommerce.Domain.Commerce.Model.AddressType.Billing))
-            {
-                var billingAddress = context.Order.Addresses.FirstOrDefault(x => x.AddressType == VirtoCommerce.Domain.Commerce.Model.AddressType.Billing);
-                retVal.CardOwner.PayerCountry = GetPaypalCountryCodeType(billingAddress.CountryCode);
-
-                retVal.CardOwner.Address = new PayPal.PayPalAPIInterfaceService.Model.AddressType();
-                retVal.CardOwner.Address.Street1 = billingAddress.Line1;
-                retVal.CardOwner.Address.Street2 = billingAddress.Line2;
-                retVal.CardOwner.Address.CityName = billingAddress.City;
-                retVal.CardOwner.Address.StateOrProvince = billingAddress.RegionName;
-
-                retVal.CardOwner.Address.Country = GetPaypalCountryCodeType(billingAddress.CountryCode);
-                retVal.CardOwner.Address.PostalCode = billingAddress.Zip;
-                retVal.CardOwner.Payer = billingAddress.Email;
-                retVal.CardOwner.PayerName = new PersonNameType();
-                retVal.CardOwner.PayerName.FirstName = billingAddress.FirstName;
-                retVal.CardOwner.PayerName.LastName = billingAddress.LastName;
-            }
-            else if (context.Order.Addresses.Any())
-            {
-                var billingAddress = context.Order.Addresses.FirstOrDefault();
-                retVal.CardOwner.PayerCountry = GetPaypalCountryCodeType(billingAddress.CountryCode);
-
-                retVal.CardOwner.Address = new PayPal.PayPalAPIInterfaceService.Model.AddressType();
-                retVal.CardOwner.Address.Street1 = billingAddress.Line1;
-                retVal.CardOwner.Address.Street2 = billingAddress.Line2;
-                retVal.CardOwner.Address.CityName = billingAddress.City;
-                retVal.CardOwner.Address.StateOrProvince = billingAddress.RegionName;
-
-                retVal.CardOwner.Address.Country = GetPaypalCountryCodeType(billingAddress.CountryCode);
-                retVal.CardOwner.Address.PostalCode = billingAddress.Zip;
-                retVal.CardOwner.Payer = billingAddress.Email;
-                retVal.CardOwner.PayerName = new PersonNameType();
-                retVal.CardOwner.PayerName.FirstName = billingAddress.FirstName;
-                retVal.CardOwner.PayerName.LastName = billingAddress.LastName;
-            }
-            else
-            {
-                throw new NullReferenceException("no billing address");
-            }
-
-            return retVal;
-        }
-
-        private CountryCodeType GetPaypalCountryCodeType(string threeLetterCountryCode)
-        {
-            var regions = CultureInfo.GetCultures(CultureTypes.SpecificCultures).Select(x => new RegionInfo(x.LCID));
-            var region = regions.FirstOrDefault(x => x.ThreeLetterISORegionName.Equals(threeLetterCountryCode));
-
-            var payerCountry = CountryCodeType.US;
-            if (region != null)
-            {
-                try
-                {
-                    payerCountry = (CountryCodeType)Enum.Parse(typeof(CountryCodeType), region.TwoLetterISORegionName.ToUpperInvariant());
-                }
-                catch
-                {
-                }
-            }
-            else
-            {
-                region = regions.FirstOrDefault(x => x.TwoLetterISORegionName.Equals(threeLetterCountryCode));
-                if (region != null)
-                {
-                    try
-                    {
-                        payerCountry = (CountryCodeType)Enum.Parse(typeof(CountryCodeType), region.TwoLetterISORegionName.ToUpperInvariant());
-                    }
-                    catch
-                    {
-                    }
-                }
-            }
-            return payerCountry;
-        }
-
-        private CreditCardTypeType GetPaypalCreditCardType(string creditCardType)
-        {
-            if (String.IsNullOrEmpty(creditCardType))
-                return CreditCardTypeType.VISA;
-
-            if (creditCardType.Equals("VISA", StringComparison.InvariantCultureIgnoreCase))
-                return CreditCardTypeType.VISA;
-            if (creditCardType.Equals("MASTERCARD", StringComparison.InvariantCultureIgnoreCase))
-                return CreditCardTypeType.MASTERCARD;
-            if (creditCardType.Equals("DISCOVER", StringComparison.InvariantCultureIgnoreCase))
-                return CreditCardTypeType.DISCOVER;
-            if (creditCardType.Equals("AMEX", StringComparison.InvariantCultureIgnoreCase))
-                return CreditCardTypeType.AMEX;
-            if (creditCardType.Equals("MAESTRO", StringComparison.InvariantCultureIgnoreCase))
-                return CreditCardTypeType.MAESTRO;
-            if (creditCardType.Equals("SOLO", StringComparison.InvariantCultureIgnoreCase))
-                return CreditCardTypeType.SOLO;
-            if (creditCardType.Equals("SWITCH", StringComparison.InvariantCultureIgnoreCase))
-                return CreditCardTypeType.SWITCH;
-
-            return (CreditCardTypeType)Enum.Parse(typeof(CreditCardTypeType), creditCardType);
-        }
-
-        private string GetApiVersion()
-        {
-            return "117";
-        }
-
-        private string FormatMoney(decimal amount)
-        {
-            return amount.ToString("F2", new CultureInfo("en-US"));
-        }
-
-        private bool CheckResponse(AbstractResponseType response, out string error)
-        {
-            var retVal = false;
-
-            if (response != null)
-            {
-                if ((response.Errors != null && response.Errors.Any()) || !(response.Ack.Equals(AckCodeType.SUCCESS) || response.Ack.Equals(AckCodeType.SUCCESSWITHWARNING)))
-                {
-                    StringBuilder sb = new StringBuilder();
-                    foreach (var err in response.Errors)
-                    {
-                        sb.AppendLine(err.LongMessage);
-                    }
-
-                    error = sb.ToString();
-                }
-                else
-                {
-                    error = string.Empty;
-                    retVal = true;
-                }
-            }
-            else
-            {
-                error = "response in null";
-            }
-
-            return retVal;
-        }
-
-        #endregion
     }
 }
--- Paypal.Rest/ReadMe.txt
+++ Paypal.Rest/ReadMe.txt
@@ -1,1 +1,3 @@
 ﻿https://github.com/paypal/PayPal-NET-SDK/wiki/Quick-Start
+
+Create Module: Compress-Module
